e5007ae5bd2b4faae65b5d8d75191f2586aeffb5
Piootrekt <ppreform@gmail.com>
2021-01-14 12:46:03 +0000

    zad5 gotowe, koniec kolokwium

--- include/zad5.hpp
+++ include/zad5.hpp
@@ -7,3 +7,8 @@
 #include <vector>
 
 // tutaj funkcja sortujTagliatelle
+template <typename T>
+void sortujTagliatelle(T t1, T t2)
+{
+	std::sort(t1, t2, [](Tagliatelle& tag1, Tagliatelle& tag2) { return tag1.ileMaki(1) > tag2.ileMaki(1); });
+}
