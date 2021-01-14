c1489f56217853b76867d737202eed396c9f5590
Piootrekt <ppreform@gmail.com>
2021-01-14 12:36:34 +0000

    zad4 gotowe

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -3,3 +3,27 @@
 #include <vector>
 
 // tutaj funkcja obliczMake
+double obliczMake(std::vector<Tagliatelle> w)
+{
+	double M = 0.;
+	int i = 0;
+	for (auto it = w.rbegin(); it != w.rend(); it++)
+	{
+		i++;
+		M += it->ileMaki(i);
+	}
+	if (M > 100) 
+	{
+		int a = 21;
+		throw(a);
+	}
+	else if ((M > 50) && (M <= 100))
+	{
+		double b = 37.;
+		throw(b);
+	}
+	else if (M <= 50)
+	{
+		return M;
+	}
+}
